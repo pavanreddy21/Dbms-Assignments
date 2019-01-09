31c69ca17319825955069e3fb993c9445f6658d2
pavanreddy1998 <pavanreddy1998@gmail.com>
2019-01-09 14:35:45 +0530

    solution1-4

---
+++ SQL Query Assignments - Pt 1 /solution4.sql
@@ -0,0 +1,13 @@
+select * from customer 
+inner join rental on rental.customer_id=customer.customer_id
+inner join inventory on inventory.inventory_id=rental.inventory_id
+inner join film on film.film_id=inventory.film_id
+inner join film_actor on film.film_id=film_actor.film_id
+inner join actor on actor.actor_id=film_actor.actor_id
+
+inner join address on customer.address_id=address.address_id
+inner join city on address.city_id=city.city_id
+inner join country on city.country_id=country.country_id
+where actor.first_name='NICK' and actor.last_name='WAHLBERG' and country.country='Canada'
+;
+
