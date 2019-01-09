cda625e787aa7e74fac79307075fc492a427dc12
pavanreddy21 <pavanreddy1998@gmail.com>
2019-01-09 15:29:38 +0530

    solution2-2

--- SQL Query Assignments - Pt 2/solution1.sql
+++ SQL Query Assignments - Pt 2/solution1.sql
@@ -1,4 +1,4 @@
-select distinct count(film.film_id) from film 
+select  count(distinct film.film_id) from film 
 inner join film_category on film_category.film_id=film.film_id
 inner join category on category.category_id=film_category.category_id
 where category.name='Documentary' and film.special_features like '%Deleted Scenes%'
