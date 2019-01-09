a323fd9c0008294e5380be3c66dc589476877024
pavanreddy1998 <pavanreddy1998@gmail.com>
2019-01-09 13:17:29 +0530

    solution1-3

--- SQL Query Assignments - Pt 1 /Solution1.sql
+++ SQL Query Assignments - Pt 1 /Solution1.sql
@@ -1,4 +1,4 @@
-select * from film  
+select title from film  
 inner join film_category  on film.film_id=film_category.film_id 
 inner join category on film_category.category_id=category.category_id 
 where category.name='Comedy' and  rating='PG-13';
---
+++ SQL Query Assignments - Pt 1 /solution3.sql
@@ -0,0 +1,13 @@
+select distinct customer.*, country.country, category.NAME from customer
+inner join rental on customer.customer_id=rental.customer_id
+inner join inventory on rental.inventory_id=inventory.inventory_id
+inner join film on inventory.film_id=film.film_id 
+inner join film_category  on film.film_id=film_category.film_id 
+inner join category on film_category.category_id=category.category_id
+
+inner join address on customer.address_id=address.address_id
+inner join city on address.city_id=city.city_id
+inner join country on city.country_id=country.country_id
+where country.country='India' and  category.name='Sports'
+
+;
