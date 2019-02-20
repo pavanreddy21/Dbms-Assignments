f923b95694efab17cf4fccc7fc737b67b5f948fa
pavanreddy21 <46338498+pavanreddy21@users.noreply.github.com>
2019-02-20 22:55:44 +0530

    Update solution5.sql

--- SQL Query Assignments - Pt 2/solution5.sql
+++ SQL Query Assignments - Pt 2/solution5.sql
@@ -1,4 +1,4 @@
-select count(distinct film.film_id) from rental 
+select first_name,last_name,rating,count(distinct film.film_id) from rental 
 inner join customer on customer.customer_id=rental.customer_id
 
 inner join inventory on rental.inventory_id=inventory.inventory_id
@@ -6,4 +6,3 @@ inner join film on inventory.film_id=film.film_id
 
 where customer.first_name='susan' and customer.last_name='wilson' and  film.rating='R';
 ;
-
