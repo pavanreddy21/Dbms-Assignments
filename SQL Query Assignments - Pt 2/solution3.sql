0084f52b848d2f6cae60c5991d8e723df136be7e
pavanreddy21 <46338498+pavanreddy21@users.noreply.github.com>
2019-02-20 22:24:19 +0530

    Update solution3.sql

--- SQL Query Assignments - Pt 2/solution3.sql
+++ SQL Query Assignments - Pt 2/solution3.sql
@@ -1,4 +1,4 @@
-select  sum(payment.amount) from payment 
+select category.name,sum(payment.amount) from payment 
 inner join rental on payment.rental_id=rental.rental_id
 inner join inventory on rental.inventory_id=inventory.inventory_id
 inner join film on inventory.film_id=film.film_id 
@@ -7,6 +7,3 @@ inner join category on film_category.category_id=category.category_id
 
 
 where category.name='animation' 
-
-;
-
