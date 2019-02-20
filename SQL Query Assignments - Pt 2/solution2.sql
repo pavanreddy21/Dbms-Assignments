e78771456b0435e18ef41ccf7d46d7e2fd299b28
pavanreddy21 <46338498+pavanreddy21@users.noreply.github.com>
2019-02-20 23:18:27 +0530

    Update solution2.sql

--- SQL Query Assignments - Pt 2/solution2.sql
+++ SQL Query Assignments - Pt 2/solution2.sql
@@ -1,12 +1,11 @@
-select  count( distinct film.film_id) from film 
+select  count( distinct film.film_id),first_name,last_name from film 
 inner join film_category  on film.film_id=film_category.film_id 
 inner join category on film_category.category_id=category.category_id
 inner join inventory on inventory.film_id=film.film_id 
 
-inner join store on store.store_id=inventory.store_id
-inner join staff on staff.store_id=store.store_id
+inner join rental on rental.inventory_id=inventory.inventory_id
+inner join staff on staff.staff_id=rental.staff_id
 
 where category.name='sci-fi' and staff.first_name='jon'and staff.last_name='stephens'
 
 ;
-
