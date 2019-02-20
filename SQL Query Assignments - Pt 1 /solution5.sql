cef6aecd0e68cf0b40e5175eeee561aae0692c88
pavanreddy21 <46338498+pavanreddy21@users.noreply.github.com>
2019-02-20 18:30:48 +0530

    Update solution5.sql

--- SQL Query Assignments - Pt 1 /solution5.sql
+++ SQL Query Assignments - Pt 1 /solution5.sql
@@ -1,5 +1,4 @@
-select distinct count(title) from film
+select distinct count(title),first_name,last_name from film
 inner join film_actor on film_actor.film_id=film.film_id
 inner join actor on actor.actor_id=film_actor.actor_id
 where actor.first_name='SEAN' and actor.last_name='WILLIAMS'
-;
